135cff07916d5838dcac35aca8fecdff3f88fbb0
VicAgent <vj_plasin@hotmail.com>
2022-05-25 14:56:22 +0100

    Dockering netcore app

--- AppVentas.Infraestructura.Datos/Contextos/SaleContext.cs
+++ AppVentas.Infraestructura.Datos/Contextos/SaleContext.cs
@@ -12,6 +12,21 @@ namespace AppVentas.Infraestructura.Datos.Contextos
 {
     public class SaleContext: DbContext
     {
+
+        //private DbContext Database;
+    
+        string SERVER = Environment.GetEnvironmentVariable("SERVER");
+        string PORT = Environment.GetEnvironmentVariable("PORT");
+        string DATABASE = Environment.GetEnvironmentVariable("DATABASE");
+        string USERNAME = Environment.GetEnvironmentVariable("USERNAME");
+        string PASSWORD = Environment.GetEnvironmentVariable("PASSWORD");
+        
+
+        public SaleContext()
+        {
+            this.EnsureCreated();
+        }
+
         public DbSet<Product> Products { get; set; }
 
         public DbSet<Sale> Sales { get; set; }
@@ -21,8 +36,8 @@ namespace AppVentas.Infraestructura.Datos.Contextos
         protected override void OnConfiguring( DbContextOptionsBuilder options) 
         {    
             // crear base de datos y poner la string ( BD SQL)
-            options.UseSqlServer(@"Data source=(localdb)\MSSQLLocalDB; Initial catalog = VentasDb ; Integrated security= True;");
-
+            // options.UseSqlServer(@"Data source=(localdb)\MSSQLLocalDB; Initial catalog = VentasDb ; Integrated security= True;");
+             options.UseSqlServer($"Server={SERVER},{PORT}; Initial catalog = {DATABASE}  ; User ID={USERNAME}; Password={ PASSWORD} ;Persist Security Info= False ;Integrated security= True;");
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
